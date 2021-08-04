e95272b730444b7a1903394f4592c05e9322166b
gendeu <gendeulgagarin@gmail.com>
2021-08-04 16:38:14 +0800

    multiple sound playing

--- src/view/screens/List.js
+++ src/view/screens/List.js
@@ -1,88 +1,65 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Audio } from 'expo-av';
-
-Audio.setAudioModeAsync({ 
-  playsInSilentModeIOS: true,
-  allowsRecordingIOS: false,                                                                                                                                                                                   
-  interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS,                                                                         
-  shouldDuckAndroid: false,                                                                                                           
-  interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-  playThroughEarpieceAndroid: true
-});
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 export default function List() {
-  const [sound, setSound] = React.useState();
-
-  async function playSound1() {
-    console.log('Loading Sound');
-
-    const { sound } = await Audio.Sound.createAsync(
-       require('../../assets/Plain/C.mp3')
-    );
-    setSound(sound);
-
-    console.log('Playing Sound');
-    await sound.playAsync(); 
-  }
-
-  async function stopSound1() {
-    console.log('Stopped Sound');
-    await sound.stopAsync(); 
-  }
-
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync(); }
-      : undefined;
-  }, [sound]);
-
-
-  
-  async function playSound2() {
-    console.log('Loading Sound');
-
-    const { sound } = await Audio.Sound.createAsync(
-       require('../../assets/Victory/V.mp3')
-    );
-    setSound(sound);
-
-    console.log('Playing Sound');
-    await sound.playAsync(); 
-  }
-
-  async function stopSound2() {
-    console.log('Stopped Sound');
-    await sound.stopAsync(); 
-  }
-
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync(); 
-        }
-      : undefined;
-  }, [sound]);
+  const video1 = React.useRef(null);
+  const [status1, setStatus1] = React.useState({});
+  const video2 = React.useRef(null);
+  const [status2, setStatus2] = React.useState({});
 
   return (
     <View style={styles.container}>
-      <Button title="Play Sound" onPress = {() => {playSound1()} } />
-      <Button title="Stop Sound" onPress = {() => {stopSound1()} } />
-      <Button title="Play Sound" onPress = {() => {playSound2()} } />
-      <Button title="Stop Sound" onPress = {() => {stopSound2()} } />
+      <Video
+        ref={video1}
+        style={styles.video}
+        source={require('../../assets/Plain/C.mp4')}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status1 => setStatus1(() => status1)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status1.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status1.isPlaying ? video1.current.pauseAsync() : video1.current.playAsync()
+          }
+        />
+      </View>
+
+
+      <Video
+        ref={video2}
+        style={styles.video}
+        source={{
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status2 => setStatus2(() => status2)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status2.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status2.isPlaying ? video2.current.pauseAsync() : video2.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 10,
+
   },
-});
- 
+  video: {
+
+  },
+  buttons: {
+
+  }
+ }); 
