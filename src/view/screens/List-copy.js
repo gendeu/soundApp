11bae696d12f342dee464754cc21158a10532489
gendeu <gendeulgagarin@gmail.com>
2021-08-04 18:14:06 +0800

    dev

--- src/view/screens/List-copy.js
+++ src/view/screens/List-copy.js
@@ -1,51 +1,66 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Audio } from 'expo-av';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
-export default function App() {
-  const [sound, setSound] = React.useState();
-
-  async function playSound() {
-    console.log('Loading Sound');
-    await Audio.setAudioModeAsync({ 
-        playsInSilentModeIOS: true,
-        allowsRecordingIOS: false,                                                                                                                                                                                   
-        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS,                                                                         
-        shouldDuckAndroid: false,                                                                                                           
-        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-        playThroughEarpieceAndroid: true
-    })
-    const { sound } = await Audio.Sound.createAsync(
-       require('../../assets/Plain/C.mp3')
-    );
-    setSound(sound);
-
-    console.log('Playing Sound');
-    await sound.playAsync(); 
-  }
-
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync(); }
-      : undefined;
-  }, [sound]);
+export default function List() {
+  
+  const video1 = React.useRef(null);
+  const [status1, setStatus1] = React.useState({});
+  const video2 = React.useRef(null);
+  const [status2, setStatus2] = React.useState({});
 
   return (
     <View style={styles.container}>
-      <Button title="Play Sound" onPress={playSound} />
-      <Button title="Stop Sound" onPress={playSound} />
+      <Video
+        ref={video1}
+        style={styles.video}
+        source={require('../../assets/Plain/C.mp4')}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status1 => setStatus1(() => status1)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status1.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status1.isPlaying ? video1.current.pauseAsync() : video1.current.playAsync()
+          }
+        />
+      </View>
+
+
+      <Video
+        ref={video2}
+        style={styles.video}
+        source={{
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status2 => setStatus2(() => status2)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status2.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status2.isPlaying ? video2.current.pauseAsync() : video2.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 10,
+
   },
-});
- 
+  video: {
+
+  },
+  buttons: {
+
+  }
+ }); 
--- src/view/screens/List.js
+++ src/view/screens/List.js
@@ -1,65 +1,299 @@
-import * as React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+  Button
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import CardFlip from 'react-native-card-flip';
 import { Video, AVPlaybackStatus } from 'expo-av';
 
-export default function List() {
+import Color from '../../consts/colors';
+import { C1, C2, C3, C4, C5, C6, C7 }  from '../../consts/data';
+
+const width = Dimensions.get('window').width / 2 - 20;
+const height = Dimensions.get('window').height;
+
+export default function List({route, navigation}) {
+  const { cag } = route.params;
+
   const video1 = React.useRef(null);
   const [status1, setStatus1] = React.useState({});
   const video2 = React.useRef(null);
   const [status2, setStatus2] = React.useState({});
 
   return (
-    <View style={styles.container}>
-      <Video
-        ref={video1}
-        style={styles.video}
-        source={require('../../assets/Plain/C.mp4')}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status1 => setStatus1(() => status1)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status1.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status1.isPlaying ? video1.current.pauseAsync() : video1.current.playAsync()
-          }
-        />
-      </View>
+    <View style={stylesList.container}>
+      <ScrollView>
+      <View style={stylesList.fullWidth}>
+      
+      {
+        // Category 1
+        cag == 'C1' ?
+          C1.map((C1, index) => 
+          <View style={stylesList.item}  key={ index }>
+          <CardFlip style={stylesList.card}  ref={ (card) => this['card' + index] = card }>
+          <View style={[stylesList.card]}
+            
+          >
+              
+            <View
+              style={{
+                height: 100,
+                alignItems: 'center',
+              }}>
+              <Image
+                source={C1.img}
+                style={stylesList.image}
+              />
+              <View style={stylesList.bottomView}></View>
 
+              <View style={stylesList.contentView}>
+                <Text style={{fontSize: 22, color: "white"}}>Key of </Text> 
+                <Text style={stylesList.textStyle}>
+                  {C1.name}
+                </Text>
+                <View style={{flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+                  <TouchableOpacity style={stylesList.appButtonContainer} 
+                    onPress={() => {
+                      this['card' + index].flip();
+                    }}>
+                    <Text style={stylesList.appButtonText}>Play Now</Text>
+                  </TouchableOpacity>
+                  <View style={stylesList.appButtonContainer2}>
+                    <Text style={stylesList.appButtonText2}>
+                    <Icon
+                    name="bookmark"
+                    backgroundColor="#3b5998"
+                    // onPress={this.loginWithFacebook}
+                    >
+                    </Icon>
+                    </Text>
+                  </View>
+                </View>
+              </View>
 
-      <Video
-        ref={video2}
-        style={styles.video}
-        source={{
-          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status2 => setStatus2(() => status2)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status2.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status2.isPlaying ? video2.current.pauseAsync() : video2.current.playAsync()
-          }
-        />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 5,
+              }}>
+              
+              <Text
+                  style={{fontSize: 22, color: "white", fontWeight: 'bold'}}>
+              </Text>
+              
+            </View>
+          </View>
+          <TouchableOpacity style={[stylesList.card]}
+            onPress={() => {
+              this['card' + index].flip();
+            }}
+          >
+              
+            <View
+              style={{
+                height: 100,
+                alignItems: 'center',
+              }}>
+              <Image
+                source={require('../../assets/main.jpg')}
+                style={stylesList.image}
+              />
+              <View style={stylesList.bottomView}></View>
+
+              <View style={stylesList.contentView}>
+                <Text style={stylesList.textStyle}>
+                  {C1.name}
+                </Text>
+                <Text style={{fontSize: 13, color: "white", textAlign: 'center'}}>
+                  {C1.status}
+                </Text>
+                <Video
+                  ref={video1}
+                  source={require('../../assets/Plain/C.mp4')}
+                  useNativeControls
+                  resizeMode="contain"
+                  isLooping
+                  onPlaybackStatusUpdate={status1 => setStatus1(() => status1)}
+                />
+                <View>
+                  <Button
+                    title={status1.isPlaying ? 'Pause' : 'Play'}
+                    onPress={() =>
+                      status1.isPlaying ? video1.current.pauseAsync() : video1.current.playAsync()
+                    }
+                  />
+                </View>
+
+              </View>
+
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 5,
+              }}>
+              
+              <Text
+                  style={{fontSize: 22, color: "white", fontWeight: 'bold'}}>
+              </Text>
+              
+            </View>
+          </TouchableOpacity>
+          </CardFlip>
+        </View>
+        )
+        :
+           
+        console.log('else')
+
+      }
       </View>
+      </ScrollView>
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
+const stylesList = StyleSheet.create({
   container: {
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start' 
+  },
+  item: {
+    alignItems: 'center',
+    width: '50%',
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  categoryText: {
+    fontSize: 16, 
+    color: 'grey', 
+    fontWeight: 'bold'
+  },
+  categoryTextSelected: {
+    color: Color.dark,
+    paddingBottom: 5,
+    borderBottomWidth: 2,
+    borderColor: Color.dark,
+  },
+  card: {
+    height: height / 3 - 40, 
+    width: width, 
+    // backgroundColor: 'gray',
+    marginHorizontal: 2,
+    borderRadius: 10,
+    // padding: 15,
+    margin: 8,
 
   },
-  video: {
-
+  header: {
+    backgroundColor: "white",
+    height: 100,
+    marginTop: 30,
   },
-  buttons: {
-
+  searchContainer: {
+    height: 50,
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    flex: 1,
+    color: "brown",
+  },
+  sortBtn: {
+    marginLeft: 10,
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    backgroundColor: Color.dark,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fullWidth: {
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+  bottomView: {
+    width: '100%', 
+    height: height / 3 - 20,
+    // height: 120, 
+    backgroundColor: 'black', 
+    opacity: 0.5,
+    bottom: 0,
+  },
+  contentView: {
+    width: '100%', 
+    height: height / 3 - 20,
+    top: 0,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStyle: {
+    fontWeight: 'bold', 
+    color: 'white',
+    fontSize: 32,
+    padding: 10,
+    textAlign: 'center'
+  },
+  image: {
+    height: height / 3 - 50, 
+    width: width - 10, 
+    position: 'absolute', 
+    zIndex: 0
+  },
+  appButtonContainer: {
+    zIndex: 2,
+    elevation: 8,
+    backgroundColor: "#667e99",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  appButtonContainer2: {
+    zIndex: 2,
+    elevation: 8,
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginLeft: 5,
+  },
+  appButtonText: {
+    fontSize: 12,
+    color: "black",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  appButtonText2: {
+    fontSize: 12,
+    color: "black",
+    padding: 1.7,
+    alignSelf: "center",
+    textTransform: "uppercase"
   }
- }); 
+});
